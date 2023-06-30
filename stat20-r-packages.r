fde638951a1ebb15ce024b449e6d8463546c23ca
ryanlovett <rylo@berkeley.edu>
2023-06-29 20:09:37 -0700

    Add required libraries.

--- stat20-r-packages.r
+++ stat20-r-packages.r
@@ -8,11 +8,18 @@ set_libs <- c(
   "rmarkdown", "2.20",
   "plotly", "4.10.1",
   "patchwork", "1.1.2",
-  "reshape2", "1.4.4"
+  "reshape2", "1.4.4",
+  "kableExtra", "1.3.4",
+  "infer", "1.0.4",
+  "countdown", "0.4.0",
+  "palmerpenguins", "0.1.1",
+  "ggrepel", "0.9.3",
+  "ggthemes", "4.2.4"
 )
 
 set_libs_install_version(set_name, set_libs)
 
 remotes::install_github("stat20/stat20data")
+remotes::install_github("hadley/emo")
 
 print(paste("Done installing packages for", set_name))
